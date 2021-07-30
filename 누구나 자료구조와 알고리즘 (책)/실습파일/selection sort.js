300b2f5498420fb51bdbffc1fcbef7cd61018a39
dragoocho <deagoocho@gmail.com>
2021-07-30 20:57:31 +0900

    image 업데이트

--- 누구나 자료구조와 알고리즘 (책)/실습파일/selection sort.js
+++ 누구나 자료구조와 알고리즘 (책)/실습파일/selection sort.js
@@ -15,36 +15,36 @@
 //   return array;
 // }
 
-const answers = [1, 2, 3, 4, 5];
+// const answers = [1, 2, 3, 4, 5];
 
-function solution(answers) {
-  var answer = [];
-  var a = 0;
-  var b = 0;
-  var c = 0;
-  var mans = [a, b, c];
-  var queS = [
-    [1, 2, 3, 4, 5],
-    [2, 1, 2, 3, 2, 4, 2, 5],
-    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
-  ];
+// function solution(answers) {
+//   var answer = [];
+//   var a = 0;
+//   var b = 0;
+//   var c = 0;
+//   var mans = [a, b, c];
+//   var queS = [
+//     [1, 2, 3, 4, 5],
+//     [2, 1, 2, 3, 2, 4, 2, 5],
+//     [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+//   ];
 
-  for (let i = 0; i < mans.length; i++) {
-    let count = 0;
-    for (let j = 0; j < answers.length; j++) {
-      if (answers[j] === queS[i][j]) {
-        count += 1;
-        console.log(mans[j]);
-      }
-    }
-    mans[i] = count;
-  }
+//   for (let i = 0; i < mans.length; i++) {
+//     let count = 0;
+//     for (let j = 0; j < answers.length; j++) {
+//       if (answers[j] === queS[i][j]) {
+//         count += 1;
+//         console.log(mans[j]);
+//       }
+//     }
+//     mans[i] = count;
+//   }
 
-  mans.sort((a, b) => a - b);
+//   mans.sort((a, b) => a - b);
 
-  answer = [...mans];
+//   answer = [...mans];
 
-  return console.log(answer);
-}
+//   return console.log(answer);
+// }
 
-solution(answers);
+// solution(answers);
