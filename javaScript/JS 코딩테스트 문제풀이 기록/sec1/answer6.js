aeec36ccc06082cf23f0a02b7c9b80c3d38f8380
dragoocho <deagoocho@gmail.com>
2021-09-16 23:54:49 +0900

    js 코딩테스트 강의풀이 복습

--- javaScript/JS 코딩테스트 문제풀이 기록/sec1/answer6.js
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec1/answer6.js
@@ -1,11 +1,9 @@
 function solution(arr) {
-  let minNum = 0,
-    sumNum = 0,
-    oddNumArr = [];
+  let [minNum, sumNum, oddNumArr] = [0, 0, []];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 1) {
-      oddNumArr.push(arr[i]);
+  for (let e of arr) {
+    if (e % 2 === 1) {
+      oddNumArr.push(e);
     }
   }
 
@@ -20,6 +18,8 @@ function solution(arr) {
 let arr = [12, 77, 38, 41, 53, 92, 85];
 console.log(solution(arr));
 
-// ES6 Spread Operator 할당(...oddNumArr)과
-// 템플릿 리터럴`${sumNum}\n${minNum}`를 활용하면
+// ES6 for of 반복문 "for (let e of arr) {}" 과
+// 디스트럭쳐링 할당 "let [minNum, sumNum, oddNumArr] = [0, 0, []]" 과
+// Spread Operator 할당(...oddNumArr) 과
+// 템플릿 리터럴 `${sumNum}\n${minNum}`를 활용하면
 // 코드가 간단하고 읽기 좋아진다.
