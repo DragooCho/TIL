4ef80993e994f62fff4728dc3304ebd01813043b
dragoocho <deagoocho@gmail.com>
2021-09-18 08:34:07 +0900

    js 코딩테스트 강의풀이 복습

--- javaScript/JS 코딩테스트 문제풀이 기록/sec1/answer6.js
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec1/answer6.js
@@ -20,6 +20,6 @@ console.log(solution(arr));
 
 // ES6 for of 반복문 "for (let e of arr) {}" 과
 // 디스트럭쳐링 할당 "let [minNum, sumNum, oddNumArr] = [0, 0, []]" 과
-// Spread Operator 할당(...oddNumArr) 과
+// Spread Operator 할당 (...oddNumArr) 과
 // 템플릿 리터럴 `${sumNum}\n${minNum}`를 활용하면
 // 코드가 간단하고 읽기 좋아진다.
