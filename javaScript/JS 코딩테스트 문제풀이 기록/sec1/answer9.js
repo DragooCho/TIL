71e34ad8437227a2a28c34dd66120192eb46a9fc
dragoocho <deagoocho@gmail.com>
2021-09-19 15:42:33 +0900

    js 코딩테스트 강의풀이 복습

---
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec1/answer9.js
@@ -0,0 +1,35 @@
+function solution(s) {
+  // let newArr = [];
+  // let answer = "";
+  // for (let i of arr) {
+  //   if (i === "A") {
+  //     newArr.push("#");
+  //   } else {
+  //     newArr.push(i);
+  //   }
+  // }
+
+  // answer = newArr.reduce((a, b) => a + b);
+
+  ///////////////////////
+
+  let answer = "";
+
+  for (let i of s) {
+    if (i === "A") {
+      answer += "#";
+    } else {
+      answer += i;
+    }
+  }
+  // forEach문은 배열에 적용 가능하지만 문자열에서는 쓸 수없다.
+
+  //////////////////////////////////////////////////////////////
+
+  // s.replace(' ', 'monkey')
+
+  return answer;
+}
+
+let str = "BANANA";
+console.log(solution(str));
