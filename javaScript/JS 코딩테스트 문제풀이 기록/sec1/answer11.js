8b2312f7da306b8bab8e1446bf1a2121e40ecb46
dragoocho <deagoocho@gmail.com>
2021-09-20 10:50:15 +0900

    js 코딩테스트 강의풀이 복습

--- javaScript/JS 코딩테스트 문제풀이 기록/sec1/answer11.js
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec1/answer11.js
@@ -1,7 +1,11 @@
 function solution(s) {
   let answer = 0;
+  // for (let i of s) {
+  //   if (i === i.toUpperCase()) answer++;
+  // }
   for (let i of s) {
-    if (i === i.toUpperCase()) answer++;
+    let num = i.charCodeAt();
+    if (num >= 65 && num <= 90) answer++;
   }
   return answer;
 }
