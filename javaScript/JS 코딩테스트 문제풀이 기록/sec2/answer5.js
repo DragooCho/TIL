a5dc9951af1ed079707d945b1345fde98915a046
dragoocho <deagoocho@gmail.com>
2021-09-24 01:30:38 +0900

    js 코딩테스트 강의풀이 복습

--- javaScript/JS 코딩테스트 문제풀이 기록/sec2/answer5.js
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec2/answer5.js
@@ -1,29 +1,31 @@
-// function solution(arr) {
-//   let answer = Math.min(...arr);
-
-//   return answer;
-// }
-// let arr = [51, 31, 7, 11, 21, 15, 17];
-
-// console.log(solution(arr));
-
 function solution(arr) {
-  let answer,
-    min = Number.MAX_SAFE_INTEGER;
+  // let answer = [];
+  // for (let i of arr) {
+  //   let n = 1;
+  //   for (let j of arr) {
+  //     if (j > i) {
+  //       n++;
+  //     }
+  //   }
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
+  //   answer.push(n);
+  // }
+
+  let n = arr.length;
+  answer = Array.from({ length: n }, () => 1);
+  // Array.from()으로 배열을 생성한 후
+  // 배열의 길이와 각 요소들에게 연산이 가능하다.
+
+  for (let i of arr) {
+    for (let j = 0; j < n; j++) {
+      if (arr[j] < i) {
+        answer[j]++;
+      }
     }
   }
 
-  answer = min;
-
   return answer;
 }
-let arr = [51, 31, 7, 11, 21, 15, 17];
+let arr = [87, 89, 92, 100, 76];
 
 console.log(solution(arr));
-
-// Number.MAX_SAFE_INTEGER는 JavaScript에서 안전한 최대 정수값을 나타냅니다.
-// Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2는 참으로 평가되며 이는 수학적으로 올바르지 않습니다.
