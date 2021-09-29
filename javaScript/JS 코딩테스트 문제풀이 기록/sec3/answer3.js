e4ad576d4a5549ff5ec671d4c6d9785aa27b4568
dragoocho <deagoocho@gmail.com>
2021-09-29 12:40:06 +0900

    js 코딩테스트 강의풀이 복습

--- javaScript/JS 코딩테스트 문제풀이 기록/sec3/answer3.js
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec3/answer3.js
@@ -1,7 +1,23 @@
-function solution(arr) {
-  let answer = [];
+function solution(str) {
+  // let answer = "";
+
+  // for (let e of str) {
+  //   if (!isNaN(e)) answer += e;
+  // }
+
+  // return parseInt(answer, 10);
+
+  let answer = "";
+
+  for (let e of str) {
+    if (0 <= e && e < 10) answer += e;
+    // isNaN()은 숫자 NaN의 참거짓을 반환하는 함수이다.
+    // 이런 함수를 쓰는게 아니면 보통의 숫자 범위를 파악하는 조건문도 된다.
+  }
+
+  return parseInt(answer);
 }
 
-let array = [];
+let str = "g0en2T0s8eSoft";
 
-console.log(solution(array));
+console.log(solution(str));
