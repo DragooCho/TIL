61f1715d58a3fdebe15421d0e2fd72124fcf1633
dragoocho <deagoocho@gmail.com>
2021-09-29 14:54:41 +0900

    js 코딩테스트 강의풀이 복습

--- javaScript/JS 코딩테스트 문제풀이 기록/sec3/answer4.js
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec3/answer4.js
@@ -1,7 +1,48 @@
-function solution(arr) {
+function solution(str, s) {
   let answer = [];
+  let leng = 100;
+
+  for (let i of str) {
+    if (i === s) {
+      leng = 0;
+      answer.push(leng);
+    } else {
+      leng++;
+      answer.push(leng);
+    }
+  }
+
+  leng = 100;
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] === s) {
+      leng = 0;
+      answer[i] = leng;
+    } else {
+      leng++;
+      answer[i] = Math.min(answer[i], leng);
+    }
+  }
+
+  // let numberS = [];
+
+  // for (let i = 0; i < str.length; i++) {
+  //   if (str[i] === s) numberS.push(str.indexOf(s, i));
+  // }
+
+  // for (let i = 0; i < str.length; i++) {
+  //   let min = str.length;
+
+  //   for (let j of numberS) {
+  //     let n = Math.abs(i - j);
+  //     if (n < min) min = n;
+  //   }
+
+  //   answer += min + " ";
+  // }
+
+  return answer;
 }
 
-let array = [];
+let str = "teachermodefhgfj";
 
-console.log(solution(array));
+console.log(solution(str, "e"));
