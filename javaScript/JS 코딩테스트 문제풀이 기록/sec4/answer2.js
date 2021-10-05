bd22b324c7d8d4d7da85e94f8ddf897c4340360a
dragoocho <deagoocho@gmail.com>
2021-10-05 12:06:19 +0900

    js 코딩테스트 강의풀이 복습

--- javaScript/JS 코딩테스트 문제풀이 기록/sec4/answer2.js
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec4/answer2.js
@@ -1,17 +1,34 @@
 function solution(arr) {
   let answer = [];
-  let sosu = [2, 3, 5, 7];
+  // let sosu = [2, 3, 5, 7];
+
+  // for (let e of arr) {
+  //   let nSum = Number(String(e).split("").reverse().join(""));
+
+  //   let count = 0;
+
+  //   for (let i of sosu) {
+  //     if (nSum === i || (nSum % i !== 0 && nSum !== 1)) count++;
+  //   }
+
+  //   if (count === 4) answer.push(nSum);
+  // }
+
+  function sosu(n) {
+    if (n === 1) return false;
+    // 순회 하기 전에 미리 1를 검사한다.
+    for (let i = 2; i * i <= n; i++) {
+      if (n % i === 0) return false;
+    }
+    // 루트값을 이용하면 시간복잡도는 O(√N)이 된다.
+    // 루트함수를 사용하지않고 i의 제곱값으로도 계산이 가능하다.
+    return true;
+  }
 
   for (let e of arr) {
     let nSum = Number(String(e).split("").reverse().join(""));
 
-    let count = 0;
-
-    for (let i of sosu) {
-      if (nSum === i || (nSum % i !== 0 && nSum !== 1)) count++;
-    }
-
-    if (count === 4) answer.push(nSum);
+    if (sosu(nSum)) answer.push(nSum);
   }
 
   return answer;
--- javaScript/JS 코딩테스트 문제풀이 기록/sec4/answer3.js
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec4/answer3.js
@@ -1,19 +1,51 @@
-// function solution(arr, s) {
-//   let answer = [];
+function solution(arr) {
+  let answer = 0;
+  let n = arr[0].length;
+  // (i,j), arr[y][x]
 
-//   return answer;
-// }
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      // i,j 멘토 멘티의 경우의 수를 차례대로 순회하되
+      // 그 안에서 또 다시 배열의 순회를 거친다.
 
-// let arr = [];
+      let testCounters = 0;
+      // 배열을 모두 순회하면 임시카운트를 초기화하는 위치
 
-// console.log(solution(arr, "e"));
-let num = 6;
+      for (let y = 0; y < arr.length; y++) {
+        let mTo,
+          mTi = 0;
+        // 배열을 순회하는 반복문 안에서 맨토, 멘티를 초기화하는 위치
 
-function isPrime(num) {
-  for (let i = 2; i <= num; i++) {
-    if (num >= 2 && num % i !== 0) return true;
-    console.log(i);
+        for (let x = 1; x <= n; x++) {
+          if (arr[y][x] === i) mTo = x;
+          if (arr[y][x] === j) mTi = x;
+        }
+        if (mTo > mTi) testCounters++;
+        // 배열 y를 순회하면서 맨토, 멘티를 구별하고
+        // 맨티보다 맨토의 값이 큰 경우를 임시로 카운트한다.
+      }
+
+      if (testCounters === arr.length) answer++;
+      // 임시카운트 숫자가 배열의 길이와 같다면 정식카운트에 넣는다.
+    }
   }
+
+  return answer;
 }
 
-console.log(isPrime(num));
+// let arr = [
+//   [3, 4, 1, 2],
+//   [4, 3, 2, 1],
+//   [3, 1, 4, 2],
+// ];
+let arr = [
+  [3, 1, 4, 2],
+  [1, 4, 3, 2],
+  [1, 2, 4, 3],
+];
+// let arr = [
+//   [1, 2, 3, 4, 5],
+//   [5, 4, 3, 2, 1],
+// ];
+
+console.log(solution(arr));
