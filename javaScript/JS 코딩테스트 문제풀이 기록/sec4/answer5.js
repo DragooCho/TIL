623fb4e47e1b5f1ae9c154e6bb6e3be46e0fb8cd
dragoocho <deagoocho@gmail.com>
2021-10-11 12:03:28 +0900

    js 코딩테스트 강의풀이 복습중

--- javaScript/JS 코딩테스트 문제풀이 기록/sec4/answer5.js
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec4/answer5.js
@@ -1,9 +1,58 @@
-function solution(arr, s) {
-  let answer = [];
+// function solution(arr, n) {
+//   let answer = 0;
+//   let sum = 0;
+
+//   arr.sort((a, b) => b - a);
+
+//   console.log(arr);
+
+//   let max = arr[0];
+//   let count = n;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== max) {
+//       sum += arr[i-1];
+//       max = arr[i];
+//     }
+//     // arr.shift();
+//   }
+
+//   sum += arr[n - 1];
+
+//   console.log(arr, sum);
+
+//   return answer;
+// }
+
+// let arr = [13, 15, 34, 45, 45, 65, 65, 11, 26, 42];
+
+// console.log(solution(arr, 3));
+
+function solution(arr, n) {
+  let answer = 0;
+  let sum = 0;
+
+  arr.sort((a, b) => b - a);
+
+  let max = arr[0];
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== max && count < n - 1) {
+      console.log(arr[i - 1]);
+      sum += arr[i - 1];
+      max = arr[i];
+      count++;
+    }
+
+    // arr.shift();
+  }
+
+  // sum += arr[n - 1];
+
+  console.log(arr, sum);
 
   return answer;
 }
 
-let arr = [];
+let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
 
-console.log(solution(arr, "e"));
+console.log(solution(arr, 3));
