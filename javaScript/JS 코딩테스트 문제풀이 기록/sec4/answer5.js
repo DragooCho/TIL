cebe1928c7999e9bf78e2066a9ed443d2acb1be2
dragoocho <deagoocho@gmail.com>
2021-10-14 00:16:54 +0900

    js 코딩테스트 강의풀이 복습중

--- javaScript/JS 코딩테스트 문제풀이 기록/sec4/answer5.js
+++ javaScript/JS 코딩테스트 문제풀이 기록/sec4/answer5.js
@@ -27,9 +27,10 @@
 
 // console.log(solution(arr, 3));
 
-function solution(n, k, arr) {
+function solution(k, arr) {
   let answer = 0;
   let temp = new Set();
+  let n = arr.length;
   // 배열 안의 중복숫자를 제거하기 위해
   // new Set()으로 겍체생산연산자를 만든다. 즉 여기서는 배열이 아닌 겍체로 변환된다.
 
@@ -38,6 +39,7 @@ function solution(n, k, arr) {
       for (let k = j + 1; k < n; k++) {
         temp.add(arr[i] + arr[j] + arr[k]);
         // add()는 Set겍체에 값을 집어 넣을때 유용하다.
+        // 또한 Set()특성에 의해 들어온 합산값에서 같은 합산값은 제거된다.
       }
     }
   }
@@ -52,4 +54,4 @@ function solution(n, k, arr) {
 
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
 
-console.log(solution(10, 3, arr));
+console.log(solution(3, arr));
